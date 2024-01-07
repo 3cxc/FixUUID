41929cc8ebc214c18d8e770753457487a1efa2ec
3cxc <81449257+3cxc@users.noreply.github.com>
2024-01-07 23:56:33 +0800

    删除白名单命令接管模式下的on和off指令
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- src/main/java/com/github/fixuuid/Whitelist/Command.java
+++ src/main/java/com/github/fixuuid/Whitelist/Command.java
@@ -17,10 +17,12 @@ import static com.github.fixuuid.FixUUID.plugin;
 public class Command implements CommandExecutor, TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String s, String[] args) {
-        if (command.getName().equalsIgnoreCase("whitelist")){//命令
+        if (command.getName().equalsIgnoreCase("whitelist")){
+            //仅使用/whitelist时的提示
             if (args.length == 0){
-                sender.sendMessage(ChatColor.GRAY+"/whitelist on|off|add|remove|reload|list");
+                sender.sendMessage(ChatColor.GRAY+"/whitelist add|remove|reload|list");
             }
+
             if (args.length == 1){
                 if (args[0].equalsIgnoreCase("reload")){//reload指令
                     if (checkPlayerPermission(sender,"fixuuid.whitelist.reload",ChatColor.RED+"您没有权限执行此命令！"))return false;
@@ -28,27 +30,6 @@ public class Command implements CommandExecutor, TabExecutor {
                     plugin.reloadConfig();
                     new ConfigManager().loadConfig();
                     sender.sendMessage(ChatColor.WHITE+"已重新读取配置文件！");
-                }
-                if (args[0].equalsIgnoreCase("on")){//开启白名单
-                    if (checkPlayerPermission(sender,"fixuuid.whitelist.on",ChatColor.RED+"您没有权限执行此命令！"))return false;
-                    //检查白名单是否开启
-                    if (plugin.getConfig().getBoolean("Enable",true)){
-                        sender.sendMessage(ChatColor.RED+"白名单原已开启");
-                    }else {
-                        plugin.getConfig().set("Enable",true);
-                        plugin.saveConfig();
-                        sender.sendMessage(ChatColor.WHITE+"白名单已开启");
-                    }
-                }
-                if (args[0].equalsIgnoreCase("off")){
-                    if (checkPlayerPermission(sender,"fixuuid.whitelist.off",ChatColor.RED+"您没有权限执行此命令！"))return false;
-                    if (plugin.getConfig().getBoolean("Enable",true)){
-                        plugin.getConfig().set("Enable",false);
-                        plugin.saveConfig();
-                        sender.sendMessage(ChatColor.WHITE+"白名单已关闭");
-                    }else {
-                        sender.sendMessage(ChatColor.RED+"白名单原已关闭");
-                    }
                 }
                 if (args[0].equalsIgnoreCase("list")){
                     if (checkPlayerPermission(sender,"fixuuid.whitelist.list",ChatColor.RED+"您没有权限执行此命令！"))return false;
@@ -59,9 +40,10 @@ public class Command implements CommandExecutor, TabExecutor {
                     }
                 }
                 if (args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("remove")){
-                    sender.sendMessage(ChatColor.RED+"输入的参数有误！");
+                    sender.sendMessage(ChatColor.RED+"请指定一个有效的玩家名！");
                 }
             }
+
             if (args.length == 2){
                 if (args[0].equalsIgnoreCase("add")){
                     if (checkPlayerPermission(sender,"fixuuid.whitelist.add",ChatColor.RED+"您没有权限执行此命令！"))return false;
@@ -69,8 +51,7 @@ public class Command implements CommandExecutor, TabExecutor {
                         sender.sendMessage(ChatColor.RED+"此玩家已在白名单内");
                     }else {
                         Player_Whitelist.add(args[1]);
-                        plugin.getConfig().set("Whitelist",Player_Whitelist);
-                        plugin.saveConfig();
+                        setConfig("Whitelist.list",Player_Whitelist);
                         sender.sendMessage(ChatColor.WHITE+"已添加玩家"+args[1]+"到白名单内");
                     }
                 }
@@ -80,8 +61,7 @@ public class Command implements CommandExecutor, TabExecutor {
                         sender.sendMessage(ChatColor.RED+"此玩家不在白名单内");
                     }else {
                         Player_Whitelist.remove(args[1]);
-                        plugin.getConfig().set("Whitelist",Player_Whitelist);
-                        plugin.saveConfig();
+                        setConfig("Whitelist.list",Player_Whitelist);
                         sender.sendMessage(ChatColor.WHITE+"已从白名单内删除玩家"+args[1]);
                     }
                 }
@@ -122,9 +102,9 @@ public class Command implements CommandExecutor, TabExecutor {
     private static List<String> getPlayerList() {
         ArrayList<Player> OnlinePlayerList = new ArrayList<>(Bukkit.getOnlinePlayers());
         List<String> list = new ArrayList<>();
-        for (int i = 0 ; i < OnlinePlayerList.size() ; i++){
-            if (!Player_Whitelist.contains(OnlinePlayerList.get(i).getName())){//检测玩家是否在白名单内，不在则把该玩家添加到要返回的List中
-                list.add(OnlinePlayerList.get(i).getName());
+        for (Player player : OnlinePlayerList) {
+            if (!Player_Whitelist.contains(player.getName())) {//检测玩家是否在白名单内，不在则把该玩家添加到要返回的List中
+                list.add(player.getName());
             }
         }
         return list;
@@ -147,5 +127,15 @@ public class Command implements CommandExecutor, TabExecutor {
             }
         }
         return false;
+    }
+
+    /**
+     * 更改并保存一个配置文件项
+     * @param path 配置文件项
+     * @param del 新值
+     */
+    private static void setConfig(String path,List<String> del){
+        plugin.getConfig().set(path,del);
+        plugin.saveConfig();
     }
 }
